f106705f3cc69775279ff9f294c6a887b105a8d0
Ciaran Farley <ciaran@cturtle98.com>
2022-09-26 21:18:01 -0700

    Update setup.sh
    ansible setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
 # https://github.com/cTurtle98/servers.cturtle98.net
 # setup.sh
 # Ciaran Farley
+
+
+echo "updating package lists..."
+
+apt update
+
+echo "installing python pip..."
+
+apt install python3-pip
+
+echo "installing ansible..."
+
+pip3 install ansible
+
+echo "DONE"
